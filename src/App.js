cf46ee6646823f9222209c0c43e9f01c94728b73
Shiyoki <shiyokicash1@gmail.com>
2021-10-06 18:55:31 -0400

    Four

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from './views/Home';
 import LogIn from './views/Login';
 import Notfound from './components/404';
 import Admin from './views/Admin';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
